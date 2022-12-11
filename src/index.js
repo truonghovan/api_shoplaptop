c626acc9fba571705399e46ec850be4753a352a0
VTruong <41617843+truonghovan@users.noreply.github.com>
2022-12-11 13:59:06 +0700

    hehe

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const server = require('http').createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:3002'],
+        origin: ['https://laptopshopv1.netlify.app', 'https://adminshoplaptop.netlify.app'],
     },
 })
 io.on('connection', (socket) => {
