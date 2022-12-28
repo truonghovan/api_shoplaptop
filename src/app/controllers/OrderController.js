d742d63f6c91e9322041266af614c417e898c8cc
VTruong <41617843+truonghovan@users.noreply.github.com>
2022-12-28 22:20:12 +0700

    fix

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -120,18 +120,10 @@ class OrderController {
                     console.log(error)
                     if (error) return res.status(400).json({ error })
                     if (order) {
-                        Address.findOne({
-                            user: req.user.id,
-                        }).exec((error, address) => {
-                            if (error) return res.status(400).json({ error })
-                            order.address = address.address.find(
-                                (adr) =>
-                                    adr._id.toString() ==
-                                    order.addressId.toString()
-                            )
+                       
                             res.status(200).json({
-                                order,
-                            })
+                                order
+                          
                         })
                     }
                 })
